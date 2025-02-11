9f54fd3bd4a4a6accc1b349a362bccd4f52b5b22
Ayush Gujjar <ayushprajapati61203@gmail.com>
2025-02-11 12:32:22 +0530

    first commit

---
+++ src/routes/dashboard/page.jsx
@@ -0,0 +1,21 @@
+import { Area, AreaChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+
+import { useTheme } from "@/hooks/use-theme";
+
+
+import { Footer } from "@/layouts/footer";
+
+const DashboardPage = () => {
+    const { theme } = useTheme();
+
+    return (
+        <div className="flex flex-col gap-y-4">
+            <h1 className="title">Dashboard</h1>
+           
+             
+            
+        </div>
+    );
+};
+
+export default DashboardPage;
